e940e1f6d7351db203644dc339695a44f00f3a4d
alxngoga-dev <alxngoga@gmail.com>
2026-06-15 11:42:24 +0200

    sixth commit

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -3,30 +3,22 @@ import { IoSearchOutline, IoPersonOutline } from "react-icons/io5";
 import { LiaShoppingBagSolid } from "react-icons/lia";
 import { RiArrowDropDownLine } from "react-icons/ri";
 
-
 function Navbar() {
   const [openMenu, setOpenMenu] = useState(null);
   const [mobileMenu, setMobileMenu] = useState(false);
 
-
-
-
   return (
     <>
       <div className="navcontainer">
-
         <div>
           <span className="logo">miniture</span>
         </div>
 
-
-
         <ul className={`navlink ${mobileMenu ? "active" : ""}`}>
-
-
+          
           <li
             className="list"
-            onMouseEnter={() => setOpenMenu("home")}
+             onMouseEnter={() => setOpenMenu("Home")}
             onMouseLeave={() => setOpenMenu(null)}
           >
             <div className="nav-item">
@@ -36,60 +28,57 @@ function Navbar() {
 
             {openMenu === "home" && (
               <div className="dropdown">
-                <p>Home v1 — Modern Elegance</p>
-                <p>Home v2 — Rustic Comfort</p>
-                <p>Home v3 — Minimalist Chic</p>
-                <p>Home v4 — Industrial Vibes</p>
-                <p>Home v5 — Vintage Revival</p>
-                <p>Home v6 — Kid’s Playland</p>
+                <a href="/home-v1">Home v1 — Modern Elegance</a>
+                <a href="/home-v2">Home v2 — Rustic Comfort</a>
+                <a href="/home-v3">Home v3 — Minimalist Chic</a>
+                <a href="/home-v4">Home v4 — Industrial Vibes</a>
+                <a href="/home-v5">Home v5 — Vintage Revival</a>
+                <a href="/home-v6">Home v6 — Kid’s Playland</a>
               </div>
             )}
           </li>
 
-
+       
           <li
             className="list"
             onMouseEnter={() => setOpenMenu("catalog")}
             onMouseLeave={() => setOpenMenu(null)}
           >
             <div className="nav-item">
-
               Catalog
               <RiArrowDropDownLine />
             </div>
 
             {openMenu === "catalog" && (
-
-  
               <div className="dropdown-section">
-                <div className="product-card">
-                  <img src="/Images/chairp1.png" alt="" />
+                <a href="/product/1" className="product-card">
+                  <img src="/Images/chairp1.png" alt="Chair" />
                   <h4>Ana Grey Dining Chair</h4>
                   <p>$299.99</p>
-                </div>
+                </a>
 
-                <div className="product-card">
-                  <img src="/Images/image.22.webp" alt="" />
+                <a href="/product/2" className="product-card">
+                  <img src="/Images/image.22.webp" alt="Sofa" />
                   <h4>Axis 2-Piece Sectional Sofa</h4>
                   <p>$339.99</p>
-                </div>
+                </a>
 
-                <div className="product-card">
-                  <img src="/Images/img23.png" alt="" />
+                <a href="/product/3" className="product-card">
+                  <img src="/Images/img23.png" alt="Sofa" />
                   <h4>Axis 2-Seat Sofa</h4>
                   <p>$239.99</p>
-                </div>
+                </a>
 
-                <div className="product-card">
-                  <img src="/Images/img24.png" alt="" />
+                <a href="/product/4" className="product-card">
+                  <img src="/Images/img24.png" alt="Chair" />
                   <h4>Curved Back Dining Chair</h4>
                   <p>$129.99</p>
-                </div>
+                </a>
               </div>
             )}
           </li>
 
-
+      
           <li
             className="list"
             onMouseEnter={() => setOpenMenu("pages")}
@@ -102,33 +91,27 @@ function Navbar() {
 
             {openMenu === "pages" && (
               <div className="dropdown">
-                <p>About Us</p>
-                <p>FAQs</p>
-                <p>Contact Us</p>
-
+                <a href="/about">About Us</a>
+                <a href="/faqs">FAQs</a>
+                <a href="/contact-page">Contact Us</a>
               </div>
             )}
           </li>
-
 
           <li>
             <a href="/blog">Blog</a>
           </li>
 
-
           <li>
             <a href="/contact">Contact</a>
           </li>
-
         </ul>
-
 
         <div className="nav-icons">
           <IoSearchOutline />
           <IoPersonOutline />
           <LiaShoppingBagSolid />
         </div>
-
       </div>
     </>
   );
